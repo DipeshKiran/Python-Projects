e170a74fdd05ffbe7caca96a3eadf60176fb9b5c
DipeshKiran <100759098+DipeshKiran@users.noreply.github.com>
2022-03-09 21:54:24 +0545

    Rock Paper sicssor Game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,41 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "sicssor"]
+
+while True:
+    user_input = input("Type Rock/Paper/Sicssor or Q to quit: ").lower()
+    if user_input == "q":
+        break
+    
+    if user_input not in ["rock", "paper", "sicssor"]:
+        continue
+    
+    random_number = random.randint(0, 2)
+    # rock = 0, paper = 1, sicssor = 2
+    
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+    
+    if user_input == "rock" and computer_pick == "sicssor":
+        print("You won!")
+        user_wins += 1
+    
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+
+    elif user_input == "sicssor" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+    
+    else:
+        print("You Lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("You lost", computer_wins, "times.")
+
+print("Goodbye")
